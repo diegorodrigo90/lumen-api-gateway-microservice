aa285bdfe18103805f426f73c24854055e1e90f2
Diego Rodrigo <diegorodrigo90@gmail.com>
2021-02-14 23:28:03 -0300

    handling errors

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\AuthorService;
 use App\Services\BookService;
 use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
@@ -9,6 +10,7 @@ use Illuminate\Http\Request;
 class BookController extends Controller
 {
     use ApiResponser;
+
     /**
      * The service to consume the books microservice;
      *
@@ -17,13 +19,22 @@ class BookController extends Controller
     public $bookService;
 
     /**
+     * The service to consume the author microservice;
+     *
+     * @var AuthorService
+     */
+    public $authorService;
+
+
+    /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct(BookService $bookService)
+    public function __construct(BookService $bookService, AuthorService $authorService)
     {
         $this->bookService = $bookService;
+        $this->authorService = $authorService;
     }
 
     /**
@@ -43,6 +54,7 @@ class BookController extends Controller
      */
     public function store(Request $request)
     {
+        $this->authorService->obtainAuthor($request->author_id);
         return $this->successResponse($this->bookService->createBook($request->all(), Response::HTTP_CREATED));
     }
 
